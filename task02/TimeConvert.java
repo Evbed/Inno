ce4889f759ee04279c395c896761e5205a14f53a
Evgeniy Bedarev <evbed@mail.ru>
2019-09-06 08:48:07 +0700

    First

---
+++ task02/TimeConvert.java
@@ -0,0 +1,10 @@
+package task02;
+
+public class TimeConvert {
+    public static void main(String[] args) {
+        int seconds = 3600;
+        int convert = seconds / 60 / 60;
+
+        System.out.println(convert);
+    }
+}
