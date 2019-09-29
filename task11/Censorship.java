3d98f88fc4970598e59301dd97a5d04ddfcc1ddb
Evgeniy Bedarev <evbed@mail.ru>
2019-09-29 15:03:29 +0700

    11-12 homework

--- task11/Censorship.java
+++ task11/Censorship.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class Censorship {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        String userInput = scanner.nextLine();
+
+        String censoredUserInput = userInput.replaceAll(" бяка", " Вырезано цензурой");
+        System.out.println(censoredUserInput);
+
     }
 }
+
+
---
+++ task12/MyException.java
@@ -0,0 +1,7 @@
+package task12;
+
+public class MyException extends Exception {
+    public MyException(String message) {
+        super(message);
+    }
+}
