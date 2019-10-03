4105722dea73a0e46190765000e5f05e33bfe6d2
Evgeniy Bedarev <evbed@mail.ru>
2019-10-03 09:36:21 +0700

    task13

---
+++ task13/Child.java
@@ -0,0 +1,12 @@
+package task13;
+
+public class Child {
+    public static void eat(String food) throws Exception {
+        if (food.equals(Food.COOKIE.getTitle()) || food.equals(Food.CANDY.getTitle())) {
+            System.out.println("Съел за обе щеки");
+        } else {
+            throw new Exception();
+        }
+
+    }
+}
---
+++ task13/Mother.java
@@ -0,0 +1,24 @@
+package task13;
+
+import java.util.Scanner;
+
+public class Mother {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Food.menu();
+
+        String nameOfFood = scanner.nextLine();
+
+        try {
+            Child.eat(nameOfFood);
+        } catch (Exception e) {
+            System.out.println("Выплюнул еду");
+        } finally {
+            System.out.println("Спасибо, мама");
+        }
+    }
+
+
+}
+
