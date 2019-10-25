5d0d89de5c341e4f1097bebf11305625077ccb27
Evgeniy Bedarev <evbed@mail.ru>
2019-10-25 20:42:43 +0700

    25.10

---
+++ task23/Basket.java
@@ -0,0 +1,15 @@
+package task23;
+
+public interface Basket {
+    void addProduct(String product, int quantity);
+
+    void removeProduct(String product);
+
+    void updateProductQuantity(String product, int quantity);
+
+    void clear();
+
+    void getProducts();
+
+    int getProductQuantity(String product);
+}
